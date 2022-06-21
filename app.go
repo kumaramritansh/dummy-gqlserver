89fd51f003184e8e10430113cd1db1238d311564
Amritansh Kumar <amritansh.kumar@tokopedia.com>
2022-06-21 13:23:12 +0530

    Added GRPC flow

--- app.go
+++ app.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"gitHub.com/apigee/apigee-gqlserver/gqlserver/schema"
 	"github.com/graph-gophers/graphql-go/relay"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"net/http"
+	"time"
+
+	pb "gitHub.com/apigee/apigee-gqlserver/greeting"
 )
 
+// printFeature gets the feature for the given point.
+func sayHello(client pb.GreetingServiceClient, message *pb.Message) {
+	log.Printf("Say Hello with message (%v)", message.Body)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	greeting, err := client.SayHello(ctx, message)
+	if err != nil {
+		log.Fatalf("client.SayHello failed: %v", err)
+	}
+	log.Println(greeting)
+}
+
 func main() {
+	conn, err := grpc.Dial("localhost:9000", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewGreetingServiceClient(conn)
+
+	sayHello(client, &pb.Message{Body: "World!"})
+
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Status: OK")
 	})
--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module gitHub.com/apigee/apigee-gqlserver
 go 1.15
 
 require (
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/graph-gophers/graphql-go v1.4.0
-	github.com/ysugimoto/grpc-graphql-gateway v0.22.0 // indirect
+	golang.org/x/net v0.0.0-20220617184016-355a448f1bc9 // indirect
+	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
+	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad // indirect
+	google.golang.org/grpc v1.47.0
+	google.golang.org/protobuf v1.28.0
 )
